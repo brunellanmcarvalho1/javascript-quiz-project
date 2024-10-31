b19c768a4677f1e966cfe34a46c74ebd6eae81c3
ayat <abuhajayat@gmail.com>
2024-10-31 21:30:04 +0100

    first part

--- src/question.js
+++ src/question.js
@@ -1,7 +1,16 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty){
+    this.text = text; 
+    this.choices = choices;
+    this.answer = answer; 
+    this.difficulty = difficulty; 
 
-    // 2. shuffleChoices()
+  }
+ shuffleChoices(){
+    //this.choices. Math.floor(Math.random())
+    for(i=0; i >= this.choices.length; i++){
+        const shuffleMethod =  Math.floor(Math.random());
+
+    }
+ }
 }
